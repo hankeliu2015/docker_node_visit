cf4932140789cd3bf3df1de463de739676c3371e
Hank Liu <hanke.liu@gmail.com>
2020-02-06 15:11:36 -0500

    final test passed

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const client = redis.createClient({
   host: 'redis-server',     // define redis server location
   port: 6379              // redis defaul port
 });
-client.set('visits', 0);
+client.set('visits', 0);  // 0 is exist status code
 
 app.get('/', (req, res) => {
   process.exit(0);
